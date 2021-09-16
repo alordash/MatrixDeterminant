5f6c86064fedc91d1e6e2d492e3aadedce74341b
alordash <alordash@mail.ru>
2021-09-16 13:32:49 +0300

    Adding correct validation to TryCreateMatrix

---
+++ MatrixDeterminantCalculator/MatrixDeterminantCalculator/Matrix.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MatrixDeterminantCalculator {
+
+    public class Matrix {
+        private double[,] matrix;
+        public readonly int w;
+        public readonly int h;
+
+        public Matrix(double[][] nums) {
+            w = nums.Length;
+            h = nums[0].Length;
+            matrix = new double[w, h];
+            for(int x = 0; x < w; x++) {
+                for(int y = 0; y < h; y++) {
+                    matrix[x, y] = nums[x][y];
+                }
+            }
+        }
+
+        public double this[int i, int j] {
+            get { return this.matrix[i, j]; }
+            set { this.matrix[i, j] = value; }
+        }
+    }
+}
